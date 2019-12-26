8d2b50e972fc97800ccb5d5c8baa8ea518d57f20
Leonardo C. Muniz <leocontato@outlook.com.br>
2019-12-25 21:54:23 -0300

    fim de estudos

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://Leo:leo@cluster0-7va17.mongodb.net/test?retryWrites=true&w=majority",{
+    useNewUrlParser: true
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
