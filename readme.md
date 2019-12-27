aab8d157ab17c3b48aceb1f5b94341e0b78a7e39
Leonardo C. Muniz <leocontato@outlook.com.br>
2019-12-27 20:18:18 -0300

    ok

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 Projeto resultado da Semana OmniStack #6
+
+libs utilizadas:
+express
+mongoose
+multer
+nodemon
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect("mongodb+srv://Leo:leo@cluster0-7va17.mongodb.net/test?retryWri
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use('/files', express.static( path.resolve(__dirname, '..', 'tmp') ))
 
 app.use(require('./routes'));
 
