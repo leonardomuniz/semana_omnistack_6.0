5d4ef128ad5e5139e3ca10f658e146f87f0f7dbd
Leonardo C. Muniz <leocontato@outlook.com.br>
2020-01-01 23:39:05 -0300

    Estrutura do FrontEnd

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './App.css';
+
+import Main from './pages/Main/index';
+
+class App extends Component{
+  render(){
+    return <Main />;
+  }
+}
+
+
+export default App;
